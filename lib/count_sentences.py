3ae57eca89ade481abf9d8fb0ff59af3a6cbf813
Soojungkim <soojung.kimm@gmail.com>
2024-03-11 19:45:56 -0700

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+import re
 class MyString:
-  pass
+
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return True if self._value.endswith(".") else False
+
+    def is_question(self):
+        return True if self._value.endswith("?") else False
+
+    def is_exclamation(self):
+        return True if self._value.endswith("!") else False
+
+    def count_sentences(self):
+      #Using the re.split() to remove the punctuation. re.split() will look for those 'patterns' in quotation and returns
+      #remaining characters in the list.
+        sentences = [sentence for sentence in re.split('[.!?]', self.value) if sentence]
+        return len(sentences)
+        
